a4b214695d53e421faf1260d44d8488d906ff826
CameronLenartowich <camtl66@gmail.com>
2021-02-13 11:48:15 -0500

    initial commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+//pragma solidity ^0.7.4;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage)  {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
